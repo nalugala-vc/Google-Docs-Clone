551342f15e57e0434e75d152782dce3c9a8893df
nalugala-vc <vanessachebukwa@gmail.com>
2023-04-17 12:48:53 +0300

    Text editor implemented with different features

--- lib/screens/document_screen.dart
+++ lib/screens/document_screen.dart
@@ -1,5 +1,7 @@
+import 'package:amazonclone/utils/colors.dart.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_quill/flutter_quill.dart' as quil;
 
 class DocumentScreen extends ConsumerStatefulWidget {
   final String id;
@@ -10,12 +12,105 @@ class DocumentScreen extends ConsumerStatefulWidget {
 }
 
 class _DocumentScreenState extends ConsumerState<DocumentScreen> {
+  TextEditingController titleController =
+      TextEditingController(text: 'Untitled Document');
+
+  final quil.QuillController _controller = quil.QuillController.basic();
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    titleController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text(widget.id),
-      ),
-    );
+        appBar: AppBar(
+          actions: [
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: ElevatedButton.icon(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.lock,
+                  size: 16,
+                ),
+                label: const Text('share'),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: blueColor,
+                ),
+              ),
+            )
+          ],
+          title: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 9.0),
+            child: Row(
+              children: [
+                Image.asset(
+                  'assets/images/docs-logo.png',
+                  height: 40,
+                ),
+                const SizedBox(
+                  width: 10,
+                ),
+                SizedBox(
+                  width: 180,
+                  child: TextField(
+                    controller: titleController,
+                    decoration: const InputDecoration(
+                        border: InputBorder.none,
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: blueColor,
+                          ),
+                        ),
+                        contentPadding: EdgeInsets.only()),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          backgroundColor: whiteColor,
+          elevation: 0,
+          bottom: PreferredSize(
+            preferredSize: const Size.fromHeight(1),
+            child: Container(
+              decoration: BoxDecoration(
+                border: Border.all(color: greyColor, width: 0.1),
+              ),
+            ),
+          ),
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 10,
+              ),
+              quil.QuillToolbar.basic(controller: _controller),
+              const SizedBox(
+                height: 10,
+              ),
+              Expanded(
+                child: SizedBox(
+                  width: 750,
+                  child: Card(
+                    color: whiteColor,
+                    elevation: 5,
+                    child: Padding(
+                      padding: const EdgeInsets.all(30.0),
+                      child: quil.QuillEditor.basic(
+                        controller: _controller,
+                        readOnly: false, // true for view only mode
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ));
   }
 }
--- lib/utils/colors.dart.dart
+++ lib/utils/colors.dart.dart
@@ -6,3 +6,5 @@ const Color greyBackgroundCOlor = Color(0xffebecee);
 var selectedNavBarColor = Colors.cyan[800]!;
 const BlackColor = Colors.black;
 const redColor = Colors.red;
+const blueColor = Color.fromRGBO(26, 115, 232, 1);
+var greyColor = Colors.grey.shade800;
